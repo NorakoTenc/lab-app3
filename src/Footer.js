3f710054b562810b8e0e2e0d5b87f154ad0c8f0b
NorakoTenc <worldhentai69@outlook.com>
2023-11-28 00:56:17 +0200

    1

---
+++ src/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function Footer() {
+  return (
+    <div class="bottom">
+    <dl>
+      <dd>timur2003333@gmail.com</dd>
+      <dd>+380968309942</dd>
+    </dl>
+   </div>
+  );
+}
+export default Footer;
